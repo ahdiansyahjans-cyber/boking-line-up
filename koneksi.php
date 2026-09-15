ecc3c761865cc6233ec06a055cfbac839e06de86
ahdiansyahjans-cyber <ahdiansyahjans@gmail.com>
2026-09-15 22:53:31 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "dbl_rental";
+$host = getenv('MYSQLHOST') ?: "localhost";
+$user = getenv('MYSQLUSER') ?: "root";
+$pass = getenv('MYSQLPASSWORD') ?: "";
+$db   = getenv('MYSQLDATABASE') ?: "dbl_rental";
+$port = getenv('MYSQLPORT') ?: "3306";
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
